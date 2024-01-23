ca86ddf96df28b1fb4ce460cd6a78136402b583e
Devesh9446 <Srivastavadevesh43@gmail.com>
2024-01-23 22:30:05 +0530

    changes made

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,13 +1,9 @@
 import express from 'express'
 const app=express()
 
-port=process.env.PORT||8000
 
 
-import formRoutes from './routes/form.routes.js'
-app.use("api/v1/users",formRoutes)
+// import formRoutes from './routes/form.routes.js'
+// app.use("api/v1/users",formRoutes)
 
-
-app.listen(port,()=>{
-    console.log(`app is listning on port:${port}`)
-})
+export {app};
